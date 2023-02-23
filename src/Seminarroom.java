5784caf6ea339426fec4d939fb60cb02bfdfc7d7
drgnseok <dydtjr410@naver.com>
2023-02-23 18:15:55 +0900

    finla

--- src/Seminarroom.java
+++ src/Seminarroom.java
@@ -8,6 +8,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -123,36 +124,69 @@ public class Seminarroom extends JFrame {
 	public void addLabel() {
 
 		// 방 이름 라벨 - 상단에 위치
-		roomLabel = new JLabel("세미나실");
-		roomLabel.setOpaque(true);
-		roomLabel.setBounds(45, 20, 120, 30);
-		roomLabel.setBackground(Color.LIGHT_GRAY);
-		roomLabel.setFont(new Font("맑은 고딕", Font.BOLD, 20));
-		roomLabel.setHorizontalAlignment(JLabel.CENTER);
-		panel.add(roomLabel);
+				roomLabel = new JLabel();
+				// roomLabel.setOpaque(true);
+				roomLabel.setBounds(45, 20, 120, 30);
+				// roomLabel.setBackground(Color.LIGHT_GRAY);
+				roomLabel.setFont(new Font("맑은 고딕", Font.BOLD, 20));
+				roomLabel.setHorizontalAlignment(JLabel.CENTER);
+				panel.add(roomLabel);
 
-		// 오늘 날짜
-		todayLabel = new JLabel(formatedNow + " (" + dayString[dayOfWeekValue] + ")");
-		todayLabel.setOpaque(true);
-		todayLabel.setBounds(825, 50, 120, 30);
-		todayLabel.setBackground(Color.LIGHT_GRAY);
-		roomLabel.setFont(new Font("맑은 고딕", Font.BOLD, 20));
-		todayLabel.setHorizontalAlignment(JLabel.CENTER);
-		panel.add(todayLabel);
+				// 공간 라벨
+				JLabel jSpace = new JLabel("        상상관");
+				JLabel jStudio = new JLabel("❍  세미나실");
 
-		// 요일 라벨
-		for (int i = 0; i < 7; i++) {
-			dayLabel[i] = new JLabel(dayString[i]);
-			dayLabel[i].setOpaque(true);
-			dayLabel[i].setBounds(btnStartX + ((btnWidth + 3) * i), btnStartY - 70, btnWidth, btnHeight);
-			dayLabel[i].setBackground(Color.LIGHT_GRAY);
-			dayLabel[i].setFont(new Font("맑은 고딕", Font.PLAIN, 15));
-			if (i == 0)
-				dayLabel[i].setBackground(Color.RED);
-			if (i == 6)
-				dayLabel[i].setBackground(Color.BLUE);
-			dayLabel[i].setHorizontalAlignment(JLabel.CENTER);
-			this.add(dayLabel[i]);
+				// 상상관
+				panel.add(jSpace);
+				jSpace.setBounds(110, 5, 100, 100);
+				jSpace.setForeground(Color.GRAY);
+				jSpace.setFont(new Font("맑은고딕", Font.BOLD, 15));
+
+				// AI 면접실
+				panel.add(jStudio);
+				jStudio.setBounds(125, 50, 300, 70);
+				jStudio.setFont(new Font("Serif", Font.BOLD, 16));
+
+				// 상상관 옆 이미지
+				/*
+				 * JLabel imgLb3 = new JLabel(); ImageIcon thirdImg = new
+				 * ImageIcon("img03.png"); imgLb3.setIcon(thirdImg); imgLb3.setBounds(40, 30,
+				 * 40, 40); imgLb3.setHorizontalAlignment(JLabel.CENTER); panel.add(imgLb3);
+				 */
+
+				// 달력 이미지
+				JLabel imgLb4 = new JLabel();
+				ImageIcon CalendarImg = new ImageIcon("images/imgCalendar.png");
+				imgLb4.setIcon(CalendarImg);
+				imgLb4.setBounds(345, 30, 330, 330);
+				// imgCalendar.setHorizontalAlignment(JLabel.CENTER);
+				panel.add(imgLb4);
+
+				// 오늘 날짜
+				todayLabel = new JLabel(formatedNow + " (" + dayString[dayOfWeekValue] + ")");
+				todayLabel.setOpaque(true);
+				todayLabel.setBounds(825, 50, 120, 30);
+				todayLabel.setBackground(Color.WHITE);
+				roomLabel.setFont(new Font("맑은 고딕", Font.BOLD, 20));
+				todayLabel.setHorizontalAlignment(JLabel.CENTER);
+				panel.add(todayLabel);
+
+				// 요일 라벨
+
+				Color sunday = new Color(255,77,77);
+				Color saturday = new Color(102,217,255);
+				for (int i = 0; i < 7; i++) {
+					dayLabel[i] = new JLabel(dayString[i]);
+					dayLabel[i].setOpaque(true);
+					dayLabel[i].setBounds(btnStartX + ((btnWidth + 3) * i), btnStartY - 70, btnWidth, btnHeight);
+					dayLabel[i].setBackground(Color.LIGHT_GRAY);
+					dayLabel[i].setFont(new Font("맑은 고딕", Font.PLAIN, 15));
+					if (i == 0)				
+						dayLabel[i].setBackground(sunday);
+					if (i == 6)
+						dayLabel[i].setBackground(saturday);
+					dayLabel[i].setHorizontalAlignment(JLabel.CENTER);
+					this.add(dayLabel[i]);
 		}
 
 	}
